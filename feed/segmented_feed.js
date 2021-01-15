ecdc9fd8508557b24c4638b923d787fcb0c6ce1a
Olivier Mouren <olivier.mouren@inmemori.com>
2021-01-15 17:45:13 +0100

    add feed config

---
+++ feed/segmented_feed.js
@@ -0,0 +1,259 @@
+{ 
+  "0,13,m": [
+      { "t": "Les cousins", "i": "dessin-0-13-all" }
+    , { "t" : "🙏", "i" : "candle-70-130-m"}
+    , { "t" : "🙏", "i" : "candle-26-130-m"}
+    , { "t": "Laura, maman de l'école"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Famille Dubois"}
+    , { "t" : "🙏", "i" : "candle-0-13-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+
+
+
+
+  ],
+  "0,13,f": [
+      { "t": "Les cousins", "i": "dessin-0-13-all" }
+    , { "t" : "🙏", "i" : "bougie-all-f"}
+    , { "t": "Laura, maman de l'école"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Famille Dubois"}
+    , { "t" : "🙏", "i" : "candle-0-13-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+
+
+  ],
+  "14,25,m": [
+    , { "t" : "🙏", "i" : "candle-26-130-m"}
+    , { "t": "Souvenirs d'été", "i":"vacances-hossegor-25-130-all"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes amis d'enfance ✨"}
+    , { "t": "Les cousins"}
+    , { "t": "Pour mon meilleur pote"}
+    , { "t": "La bande des 3"}
+    , { "t": "Ton tonton qui t'aime  😘"}
+    , { "t": "Les anciens de la classe 5ème"}
+    , { "t": "Papa"}
+    , { "t": "Les copains du club"}
+    , { "t" : "Amis pour toujours", "i" : "shadow-14-25-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+
+
+  ],
+  "14,25,f": [
+    , { "t" : "🙏", "i" : "bougie-all-f"}
+    , { "t": "Souvenirs d'été", "i":"vacances-hossegor-25-130-all"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes amis d'enfance ✨"}
+    , { "t": "Les cousins"}
+    , { "t": "Pour ma meilleure copine"}
+    , { "t": "La bande des 3"}
+    , { "t": "Ton tonton qui t'aime 😘"}
+    , { "t": "Les anciens de la classe 5ème"}
+    , { "t": "Papa"}
+    , { "t": "Les copains du club"}
+    , { "t" : "Amis pour toujours", "i" : "shadow-14-25-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+
+  ],
+  "26,39,m": [
+    , { "t" : "Eté 2008", "i" : "lac-montagne-25-130-all"}
+    , { "t" : "🙏", "i" : "candle-70-130-m"}
+    , { "t" : "🙏", "i" : "candle-26-130-m"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes amis d'enfance✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Laura, maman de l'école"}
+    , { "t": "Les cousins"}
+    , { "t": "Maxime"}
+    , { "t": "Papa"}
+    , { "t": "Ton oncle Fred"}
+    , { "t": "RIP le plus beau 🕊"}
+    , { "t": "Un collègue à qui tu vas manquer"}
+    , { "t": "Mon frère"}
+    , { "t": "Pour mon tonton"}
+
+    , { "t" : "Ces étés vont me manquer", "i" : "tongs-26-39-all"}
+    , { "t" : "Souvenir de ce bel été ensemble", "i" : "sunset-ble-25-130-all"}
+    , { "t" : "La bande des 3", "i" : "shadow-26-39-all"}
+    , { "t" : "Voyage inoubliable avec toi", "i" : "canyons-usa-40-130-all"}
+    , { "t" : "Je n'oublierai jamais ce moment", "i" : "apero-26-39-m"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "Souvenir d'un bon dîner tous ensemble", "i" : "beers-26-39-m"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+
+
+  ],
+  "26,39,f": [
+    , { "t" : "Eté 2008", "i" : "lac-montagne-25-130-all"}
+    , { "t" : "🙏", "i" : "bougie-all-f"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes amis d'enfance✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Laura, maman de l'école"}
+    , { "t": "Les cousins"}
+    , { "t": "Maxime"}
+    , { "t": "Papa"}
+    , { "t": "Ton oncle Fred"}
+    , { "t": "RIP la plus belle 🕊"}
+    , { "t": "Un collègue à qui tu vas manquer"}
+    , { "t" :"Ma soeur"}
+    , { "t": "Pour ma tata"}
+    , { "t": "Emilie et Nicolas"}
+
+    , { "t" : "Ces étés vont me manquer", "i" : "tongs-26-39-all"}
+    , { "t" : "Souvenir de ce bel été ensemble", "i" : "sunset-ble-25-130-all"}
+    , { "t" : "La bande des 3", "i" : "shadow-26-39-all"}
+    , { "t" : "Voyage inoubliable avec toi", "i" : "canyons-usa-40-130-all"}
+    , { "t" : "Je n'oublierai jamais ce moment", "i" : "apero-26-39-m"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+
+
+
+  ],
+  "40,69,m": [
+      { "t": "Souvenirs d'été", "i":"vignes-eglise-lac-40-130-all"}
+    , { "t" : "🙏", "i" : "candle-70-130-m"}
+    , { "t" : "🙏", "i" : "candle-26-130-m"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "A mon papa ✨"}
+    , { "t": "Tes collègues à qui tu vas manquer"}
+    , { "t": "Souvenir de vacances", "i": "vacances-hossegor-25-130-all"}
+    , { "t": "Nathalie et Laurent"}
+    , { "t": "Les cousins"}
+    , { "t": "Isabelle, une voisine"}
+    , { "t": "Mon oncle"}
+    , { "t": "Emilie et Nicolas"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Corinne et Bertrand"}
+    , { "t" : "Soirée mémorable à tes côtés", "i" : "drink-40-69-all"}
+    , { "t" : "Souvenir de cette balade avec toi", "i" : "chappelle-cipres-40-130-all"}
+    , { "t" : "Voyage inoubliable avec toi", "i" : "canyons-usa-40-130-all"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+
+  ],
+  "40,69,f": [
+      { "t": "Souvenirs d'été", "i":"vignes-eglise-lac-40-130-all"}
+    , { "t" : "🙏", "i" : "candle-70-130-m"}
+    , { "t" : "🙏", "i" : "bougie-all-f"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "A ma maman ✨"}
+    , { "t": "Tes collègues à qui tu vas manquer"}
+    , { "t": "Souvenir de vacances", "i": "vacances-hossegor-25-130-all"}
+    , { "t": "Nathalie et Laurent"}
+    , { "t": "Les cousins"}
+    , { "t": "Isabelle, une voisine"}
+    , { "t": "Ma tante"}
+    , { "t": "Emilie et Nicolas"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Corinne et Bertrand"}
+    , { "t" : "Soirée mémorable à tes côtés", "i" : "drink-40-69-all"}
+    , { "t" : "Souvenir de cette balade avec toi", "i" : "chappelle-cipres-40-130-all"}
+    , { "t" : "Voyage inoubliable avec toi", "i" : "canyons-usa-40-130-all"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+
+  ],
+  "70,130,m": [
+      { "t": "Souvenirs d'été", "i":"vignes-eglise-lac-40-130-all"}
+    , { "t" : "🙏", "i" : "candle-70-130-m"}
+    , { "t" : "🙏", "i" : "candle-26-130-m"}
+    , { "i": "photo homme ancienne 1" }
+    , { "i": "photo homme ancienne 2" }
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes enfants qui t'aiment 🕊"}
+    , { "t" : "Souvenirs de Rome", "i" : "voyage-italie-40-130-all"}
+    , { "t": "Pour le meilleur des grands-pères ✨"}
+    , { "t": "Nicole et Bernard"}
+    , { "t": "Emilie, aide-soignante"}
+    , { "t": "Vacances été 2001"}
+    , { "t": "Mon plus vieil ami"}
+    , { "t": "Ta nièce adorée"}
+    , { "t": "Emilie et Nicolas"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Corinne et Bertrand"}
+    , { "t": "Sylvie, ta cousine chérie"}
+    , { "t" : "Enfance", "i" : "vintage-kid-70-130-m"}
+    , { "t" : "Souvenirs d'un bel été ensemble en Italie", "i" : "boats-holidays-40-130-all"}
+    , { "t" : "Souvenir de cette balade avec toi", "i" : "chappelle-cipres-40-130-all"}
+    , { "t" : "Unis pour la vie", "i" : "vintage-couple-70-130-all"}
+    , { "t" : "Souvenir de cet hiver", "i" : "souvenir-winter-70-130-all"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "Enfance", "i" : "vintage-boy-40-69-m"}
+    , { "t" : "Elégance en toutes circonstances", "i" : "vintage-man-70-130-m"}
+    , { "t" : "Papa ✨", "i" : "vintage-family-70-130-m"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "Notre coin de paradis", "i" : "eglise-neige-70-130-all"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+
+
+
+
+  ],
+  "70,130,f": [
+      { "t": "Souvenirs d'été", "i":"vignes-eglise-lac-40-130-all"}
+    , { "i": "illu avec une bougie" }
+    , { "t" : "🙏", "i" : "bougie-all-f"}
+    , { "t" : "Hugo", "i" : "dessin-denfant-all"}
+    , { "t": "Famille Dubois"}
+    , { "t": "Tes enfants qui t'aiment 🕊"}
+    , { "t" : "Souvenirs de Rome", "i" : "voyage-italie-40-130-all"}
+    , { "t": "Pour le meilleur des grands-pères ✨"}
+    , { "t": "Nicole et Bernard"}
+    , { "t": "Emilie, aide-soignante"}
+    , { "t": "Vacances été 2001"}
+    , { "t" : "Mon plus vieil ami"}
+    , { "t": "Ta nièce adorée"}
+    , { "t": "Emilie et Nicolas"}
+    , { "t": "Sandrine et Laurent"}
+    , { "t": "Maman ✨"}
+    , { "t": "Julie et Thomas"}
+    , { "t": "Corinne et Bertrand"}
+    , { "t": "Sylvie, ta cousine chérie"}
+    , { "t": "Annie"}
+    , { "t" : "Je pense à toi", "i" : "flower-40-130-f"}
+    , { "t" : "Souvenirs d'un bel été ensemble en Italie", "i" : "boats-holidays-40-130-all"}
+    , { "t" : "Souvenir de cette balade avec toi", "i" : "chappelle-cipres-40-130-all"}
+    , { "t" : "Unis pour la vie", "i" : "vintage-couple-70-130-all"}
+    , { "t" : "Souvenir de cet hiver", "i" : "souvenir-winter-70-130-all"}
+    , { "t" : "Je pense à toi", "i" : "etang-25-130-all"}
+    , { "t" : "Elégance en toutes circonstances", "i" : "vintage-skier-70-130-f"}
+    , { "t" : "Eté 75", "i" : "vintage-lady-beach-70-130-f"}
+    , { "t" : "Mamie ✨", "i" : "grandma-70-130-f"}
+    , { "t" : "Maman ✨", "i" : "vintage-mother-70-130-f"}
+    , { "t" : "Jeunesse", "i" : "vintage-woman-70-130-f"}
+    , { "t" : "🕊", "i" : "cloud-All"}
+    , { "t" : "Notre coin de paradis", "i" : "eglise-neige-70-130-all"}
+    , { "t" : "❤️", "i" : "pissenlit-40-130"}
+
+      ],
+    }
